9f7bd543e9bb35c41f2bec3a89f2621b261bd9c8
Roloza <romain.lozach@synomia.com>
2019-03-15 12:04:08 +0100

    Browsershot

--- src/app/components/browsershot-form/browsershot-form.component.ts
+++ src/app/components/browsershot-form/browsershot-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { ApiService } from '../../services/api.service';
+
 @Component({
   selector: 'app-browsershot-form',
   templateUrl: './browsershot-form.component.html',
@@ -12,18 +14,32 @@ export class BrowsershotFormComponent implements OnInit {
   browsershotForm = this.fb.group({
     url: ['', Validators.required]
   });
+  showLoader: boolean = false;
+  img: string;
+  errorMessage: string = "";
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
+    private apiService: ApiService,
   ) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    this.router.navigate(['browsershot/screenshot'], { queryParams: { url: this.browsershotForm.value['url'] } });
-
+    this.showLoader = true;
+    this.apiService.postBrowsershot(this.browsershotForm.value['url']).subscribe(
+      data => {
+        this.img = data.img;
+        this.showLoader = false;
+        if (data.state == "error") {
+          this.errorMessage = data.message;
+        } else{
+          this.router.navigate(['browsershot/screenshot'], { queryParams: { img: data.img } });
+        }
+      }
+    )
   }
 
 }
--- src/app/pages/browsershot/screenshot/screenshot.component.ts
+++ src/app/pages/browsershot/screenshot/screenshot.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../../services/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -9,36 +8,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ScreenshotComponent implements OnInit {
   img: string;
-  url: string;
-  errorMessage: string = "";
-  public showLoader: boolean = false;
   private sub;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private apiService: ApiService,
   ) { }
 
   ngOnInit() {
     this.sub = this.route
       .queryParams
       .subscribe(params => {
-        this.url = params['url'] || "";
-        if (this.url != "") {
-          this.img = null;
-          this.showLoader = true;
-          this.apiService.postBrowsershot(this.url).subscribe(
-            data => {
-              this.img = data.img;
-              if (data.state == "error") {
-                this.errorMessage = data.message;
-              }
-              this.showLoader = false;
-            }
-          )
-        } else {
-            this.router.navigate(["/"]);
+        this.img = params['img'] || "";
+        if (this.img == "") {
+            this.router.navigate(["browsershot"]);
         }
       });
   }
